0e53dd3720b571300a5ea1e118c8324828731132
060PM0T <Flying.rat.060PM0T@gmail.com>
2017-11-29 13:49:57 +0300

    Выводит значение функции, первой и второй производной

--- main.py
+++ main.py
@@ -3,19 +3,30 @@ import numpy as np
 from sympy import diff, symbols, cos, sin
 
 def test(pr):
-    func = pr / (pr**3 - pr + 1)
-    funcDpr1 = diff(func) #первая производная от функции
-    funcDpr2 = diff(funcDpr1) #вторая производная от функции
-    func1 = pr * funcDpr1 / func # условие A
-    if func1.real > 0: # Проверка условия A
-        func2 = 1+ rp * (funcDpr2 / funcDpr1) # условие B
-        if func2.real > 0: # Проверка условия B
-            return 1
-        else:
-            return 0
-    else:
-        return 0
+    x = 'x'
+    func = 'x / (x**3 - x + 1)'
+    x = pr
+    print(eval(str(func)))
+    x = 'x'
+    funcDpr1 = diff(func,x) #первая производная от функции
+    x = pr
+    print(eval(str(funcDpr1)))
+    x = 'x'
+    funcDpr2 = diff(funcDpr1,x) #вторая производная от функции
+    x = pr
+    print(eval(str(funcDpr2)))
     
+    func1 = pr * any(funcDpr1) / any(func) # условие A
+    if func1 != 0:        
+        if func1.real > 0: # Проверка условия A
+            func2 = 1+ rp * (funcDpr2 / funcDpr1) # условие B
+            if func2.real > 0: # Проверка условия B
+                print(1)
+            else:
+                print(0)
+        else:
+            print(0)
+        
 def build(n):
     # n - кол-во кругов, m - кол-во узлов в "круге", L - центр L=(0,0) , scale - масштаб
     dr = 1 / n  # Расстояние
@@ -29,12 +40,18 @@ def build(n):
         for y in l:
             if x**2 + y**2 < 1:
                 X.append(x)
-                Y.append(y)
+                iy = y * 1j
+                Y.append(iy)
+                
     # Отрисовка точек в круге
-    pylab.plot(X, Y, 'b.')
 
-    for
-    test()
+    for i in range(len(Y)):
+        pylab.plot(X[i], Y[i].imag, 'b.')
+
+    for i in X:
+        for j in Y:
+            test(i + j)
+
     k = np.arange(n + 1)  # Формируем массив
     alfa = k * df
     rad = n * dr
