4eaa96438040de79c4537559fc23e231d6308d08
SergeyPotrubach <SergeyPotrubach@github.com>
2017-11-29 22:18:49 +0300

    Add new def and small fix

--- main.py
+++ main.py
@@ -2,55 +2,55 @@ from matplotlib import pylab, lines, mlab
 import numpy as np
 from sympy import diff, symbols, cos, sin
 
-def test(pr):
+
+def z_x_iy(point):
+    return point.real + point.imag
+
+
+def test_conditions(pr):
     x = 'x'
     func = 'x / (x**3 - x + 1)'
+    func_d1 = diff(func, x)
+    func_d2 = diff(func_d1, x)
     x = pr
-    print(eval(str(func)))
-    x = 'x'
-    funcDpr1 = diff(func,x) #первая производная от функции
-    x = pr
-    print(eval(str(funcDpr1)))
-    x = 'x'
-    funcDpr2 = diff(funcDpr1,x) #вторая производная от функции
-    x = pr
-    print(eval(str(funcDpr2)))
-    
-    func1 = pr * any(funcDpr1) / any(func) # условие A
-    if func1 != 0:        
-        if func1.real > 0: # Проверка условия A
-            func2 = 1+ rp * (funcDpr2 / funcDpr1) # условие B
-            if func2.real > 0: # Проверка условия B
+    func_scalar = eval(str(func))
+    func_d1_scalar = eval(str(func_d1))
+    func_d2_scalar = eval(str(func_d2))
+
+    func1 = pr * z_x_iy(func_d1_scalar) / z_x_iy(func_scalar)  # условие A
+    if func1 != 0:
+        if func1.real > 0:  # Проверка условия A
+            func2 = 1 + pr * (z_x_iy(func_d2_scalar) /
+                              z_x_iy(func_d1_scalar))  # условие B
+            if func2.real > 0:  # Проверка условия B
                 print(1)
             else:
                 print(0)
         else:
-            print(0)
-        
+            print(3)
+
+
 def build(n):
-    # n - кол-во кругов, m - кол-во узлов в "круге", L - центр L=(0,0) , scale - масштаб
     dr = 1 / n  # Расстояние
     df = (2 * np.pi) / n  # Поворот
     d = 0.1
-    l = np.arange(-1,1,d)
+    l = np.arange(-2, 2, d)
     X = []
     Y = []
     # рисует точки в кружке)))
     for x in l:
         for y in l:
-            if x**2 + y**2 < 1:
+            if x**2 + y**2 <= 1:
                 X.append(x)
-                iy = y * 1j
-                Y.append(iy)
-                
+                Y.append(1j * y)
     # Отрисовка точек в круге
 
     for i in range(len(Y)):
         pylab.plot(X[i], Y[i].imag, 'b.')
 
-    for i in X:
-        for j in Y:
-            test(i + j)
+    # for i in X:
+    #     for j in Y:
+    #         test(i + j)
 
     k = np.arange(n + 1)  # Формируем массив
     alfa = k * df
@@ -59,13 +59,10 @@ def build(n):
     pylab.plot(z.real, z.imag, '-k.')
 
     pylab.axis('equal')  # Маштабирование - круг кругом
-    
+
+
 if __name__ == '__main__':
-    
     n = 25
     main_window = pylab.figure()  # Окно с графиком
     build(n)
     pylab.show()
-    
-
-    
