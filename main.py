3f01d7df461d7829902eab168b737003bb42aa84
060PM0T <Flying.rat.060PM0T@gmail.com>
2017-12-01 20:13:18 +0300

    Вроде как верно все считает...... я хз если честно

--- main.py
+++ main.py
@@ -2,63 +2,69 @@ from matplotlib import pylab, lines, mlab
 import numpy as np
 from sympy import diff, symbols, cos, sin
 
-
-def z_x_iy(point):
-    return point.real + point.imag
-
-
-def test_conditions(pr):
-    x = 'x'
-    func = 'x / (x**3 - x + 1)'
-    func_d1 = diff(func, x)
-    func_d2 = diff(func_d1, x)
+def test_conditions(pr, func, func_d1, func_d2):
     x = pr
+    print(pr)
     func_scalar = eval(str(func))
     func_d1_scalar = eval(str(func_d1))
     func_d2_scalar = eval(str(func_d2))
-
-    func1 = pr * z_x_iy(func_d1_scalar) / z_x_iy(func_scalar)  # условие A
-    if func1 != 0:
-        if func1.real > 0:  # Проверка условия A
-            func2 = 1 + pr * (z_x_iy(func_d2_scalar) /
-                              z_x_iy(func_d1_scalar))  # условие B
-            if func2.real > 0:  # Проверка условия B
-                print(1)
-            else:
-                print(0)
-        else:
-            print(3)
+    print(func_scalar)
+    
+    if func_scalar != 0:
+        func1 = pr * (func_d1_scalar / func_scalar)  # условие A
+        print (func1)
+        if func1 != 0:
+            if func1.real > 0:  # Проверка условия A
+                func2 = 1 + pr * (func_d2_scalar /
+                                  func_d1_scalar)  # условие B
+                if func2.real > 0:  # Проверка условия B
+                    #print(func2)
+                    graf1.plot(func2.real, func2.imag, '-k.')
 
 
 def build(n):
     dr = 1 / n  # Расстояние
     df = (2 * np.pi) / n  # Поворот
     d = 0.1
-    l = np.arange(-2, 2, d)
+    l = np.arange(-1, 1, d)
     X = []
     Y = []
+    graf = pylab.subplot(1,2,1)
+    global graf1
+    graf1 = pylab.subplot(1,2,2)
+
+    x = 'x'
+    func = 'x / (x + 1)'# исходная функция
+    func_d1 = diff(func, x) # производная по x
+    func_d2 = diff(func_d1, x) # вторя производная по х
+    #отображение функций
+    #print(func)
+    #print(func_d1)
+    #print(func_d2)
+    
     # рисует точки в кружке)))
     for x in l:
         for y in l:
-            if x**2 + y**2 <= 1:
+            if x**2 + y**2 < 1:
                 X.append(x)
                 Y.append(1j * y)
+
     # Отрисовка точек в круге
-
     for i in range(len(Y)):
-        pylab.plot(X[i], Y[i].imag, 'b.')
+        graf.plot(X[i], Y[i].imag, '-b.')
+        #вызов функции тест
+        test_conditions(X[i] + Y[i], func, func_d1, func_d2)
 
-    # for i in X:
-    #     for j in Y:
-    #         test(i + j)
-
+    #Точки в кручке
     k = np.arange(n + 1)  # Формируем массив
     alfa = k * df
     rad = n * dr
     z = rad * np.cos(alfa) + 1j * rad * np.sin(alfa)
-    pylab.plot(z.real, z.imag, '-k.')
+    graf.plot(z.real, z.imag, '-k.')
+    #graf1.plot(z.real, z.imag, '-k.')
 
-    pylab.axis('equal')  # Маштабирование - круг кругом
+    graf.axis('equal')
+    #graf1.axis('equal')# Маштабирование - круг кругом
 
 
 if __name__ == '__main__':
